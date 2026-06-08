479ce0cc5ae57202ed96d6ea0b855aaa4528b4c7
Juliano César <julianocesardossantosalves@gmail.com>
2026-06-08 20:37:56 -0300

    Create Aluno class with name and grade properties

---
+++ Aluno.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Aluno {
+    private String nome;
+    private double nota;
+
+    public Aluno(String nome, double nota) {
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - Nota: " + nota;
+    }
+}
